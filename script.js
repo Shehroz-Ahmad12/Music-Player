bd94cc11e658bb47ba8a448673ce4cdf94f3dd6c
Shehroz_Ahmad12 <shehrozahmad970@gmail.com>
2021-07-19 03:02:31 +0500

    First Commit Done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const image=document.getElementById('image');
 const title=document.getElementById('title');
 const anime=document.getElementById('anime');
+const backgroundImage=document.getElementById('main-img');
 
+const playerContainer=document.getElementById('player-container');
 
 const music=document.querySelector('audio');
 const prevBtn=document.getElementById('prev');
@@ -19,64 +21,87 @@ const songs=[
     {
     name: 'Crazy Noisy Bizarre Town',
     displayName: 'Crazy Noisy Bizarre Town',
-    anime: "JoJo's Bizarre Adventures"
+    anime: "JoJo's Bizarre Adventures",
+    color: 'rgb(85, 3, 153)'
     
 },
 {
     name: 'Lost in paradise',
     displayName: 'Lost In Paradise',
-    anime: "Jujutsu Kaisen" 
+    anime: "Jujutsu Kaisen" ,
+    color: 'rgb(218, 59, 138)'
 
 },
 {
     name: 'Kaikai',
     displayName: 'Kaikai Kitan Jujutsu Kaisen OP',
-    anime: "Jujutsu Kaisen" 
+    anime: "Jujutsu Kaisen" ,
+    color: "rgb(190, 5, 5)"
+
 
 },
 {
     name: "Giorno's Theme" ,
     displayName: "Giorno's Theme",
-    anime: "JoJo's Bizarre Adventures" 
+    anime: "JoJo's Bizarre Adventures",
+    color:"rgb(201, 164, 2)"
 
 },
 {
     name: 'Roundabout',
     displayName: 'Roundabout',
-    anime: "JoJo's Bizarre Adventures" 
+    anime: "JoJo's Bizarre Adventures" ,
+    color: "red"
 
 },
 {
     name: 'World that was transparent',
     displayName: 'The world that was transparent',
-    anime: "Naruto" 
+    anime: "Naruto" ,
+    color: "rgb(201, 98, 2)"
 
 },
 {
     name: 'Silhouette',
     displayName: 'Silhouette',
-    anime: "Naruto" 
+    anime: "Naruto" ,
+    color: "rgb(231, 209, 6)"
 
 },
 {
     name: 'Great Days',
     displayName: 'Great Days',
-    anime: "JoJo's Bizarre Adventures" 
-
+    anime: "JoJo's Bizarre Adventures", 
+    color: "rgb(180, 10, 75)"
 },
 {
     name: 'Kaguya sama',
     displayName: 'Kaguya-Sama OP',
-    anime: "Kaguya-Sama Love is War" 
+    anime: "Kaguya-Sama Love is War" ,
+    color: "palevioletred"
 
 },
 {
     name: 'Tokyo Ghoul',
     displayName: 'Tokyo Ghoul OP',
-    anime: "Tokyo Ghoul" 
+    anime: "Tokyo Ghoul", 
+    color:"purple"
+
+},
+{
+    name: 'Demon Slayer OP',
+    displayName: 'Demon Slayer OP',
+    anime: "Demon Slayer", 
+    color:"rgb(17, 153, 74)"
+
+},
+{
+    name: 'AOT',
+    displayName: 'Shinzo Wo Sassageyo',
+    anime: "Attack on Titan", 
+    color:"rgb(153, 83, 3)"
 
 }
-
 ];
 
 let isPlaying=false;
@@ -102,8 +127,13 @@ playBtn.addEventListener('click', () => (isPlaying ? pauseSong():playSong() ) );
 function loadSong(song){
     title.textContent=song.displayName;
     anime.textContent=song.anime;
-    music.src=`Music/${song.name}.mp3`;
+    music.src=`music/${song.name}.mp3`;
     image.src=`img/${song.name}.jpg`;
+    backgroundImage.src=`backgrounds/${song.anime}.jpg`
+
+    playerContainer.style.backgroundColor=song.color;
+    progress.style.backgroundColor=song.color;
+    progress.style.filter="brightness(50%)";
 
 }
 
@@ -157,12 +187,7 @@ function updateProgressBar(e){
 function setProgressBar(e){ 
     const width=this.clientWidth;
     const clickX=e.offsetX;
-    console.log('width',  width);
-    console.log('clickX' , clickX);
-
     const { duration } = music;
-    console.log(clickX/width);
-    console.log((clickX/width)*duration);
     music.currentTime = (clickX / width)*duration;
 }
 
@@ -175,3 +200,19 @@ nextBtn.addEventListener('click', nextSong);
 music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
 progressContainer.addEventListener('click', setProgressBar);
+
+$("#volume").slider({
+
+min: 0,
+max: 100,
+value: 75,
+range: "min",
+slide: function (event, ui) {
+    setVolume(ui.value / 100);
+}
+});
+
+function setVolume(myVolume) {
+var myMedia = document.getElementById("audio");
+myMedia.volume = myVolume;
+}
